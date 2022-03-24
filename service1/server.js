60554d83ccf6b4a66e83ad202eccf32d687daed2
kibria7533 <tenminuteversity@gmail.com>
2022-03-24 08:56:15 +0600

    hh

--- service1/server.js
+++ service1/server.js
@@ -1,12 +1,39 @@
 const express = require('express')
+var http = require("http");
 const cors=require('cors')
+var querystring = require("querystring");
 var bodyParser = require('body-parser')
 const app = express()
 const port = 8008
 app.use(cors());
 app.use(bodyParser.json())
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  var data = querystring.stringify({
+    username: "myname",
+    password: " pass"
+});
+
+var options = {
+    host: 'http://localhost',
+    port: 8009,
+    path: '/service2',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': Buffer.byteLength(data)
+    }
+};
+
+var req = http.request(options, function(res)
+{
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+        console.log("body: " + chunk);
+    });
+});
+req.write(data);
+req.end();
+  // res.send('Hello World From Service 1!')
 })
 
 app.listen(port, () => {
--- service2/server.js
+++ service2/server.js
@@ -3,11 +3,11 @@ const bodyParser=require('body-parser');
 const cors=require('cors');
 const app = express()
 const port = 8009
-
+//app1:5000
 app.use(cors());
 app.use(bodyParser.json());
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.post('/service2', (req, res) => {
+  console.log(req);
 })
 
 app.listen(port, () => {
